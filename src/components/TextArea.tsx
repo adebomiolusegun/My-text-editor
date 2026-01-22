c717c1f785c8a9f98261bbcfd5a5f2d837785d01
adebomiolusegun <adebomiolusegun1@gmail.com>
2026-01-22 17:37:32 +0100

    updating ui

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -5,7 +5,7 @@ function TextArea() {
         name=""
         id="message"
         placeholder="Write your text here..."
-        className="mt-2 w-full h-2/3 sm:w-4/5 md:w-3/4 lg:w-1/2 block p-2.5 s text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 resize-none"
+        className="mt-2 w-full h-screen sm:w-4/5 md:w-3/4 lg:w-1/2 block p-2.5 s text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 resize-none"
       ></textarea>
     </div>
   );
