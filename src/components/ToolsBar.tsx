abaf934b6ad290e1282a35ec02cbc0b0f467b8e8
adebomiolusegun <adebomiolusegun1@gmail.com>
2026-01-28 01:09:37 +0100

    removed border

--- src/components/ToolsBar.tsx
+++ src/components/ToolsBar.tsx
@@ -10,7 +10,7 @@ import Image from "@UI/toolsBarComponents/insertOptions/Image";
 function ToolsBar() {
   return (
     <div className="border-b borderBottom  ">
-      <div className="toolsbarSecDivContainer  border-2 border-red-400">
+      <div className="toolsbarSecDivContainer ">
         <div className=" flex gap-4  border-r pr-3 borderRight">
           <TextFormat id="textFormat" />
           <BulletList id="bulletList" />
